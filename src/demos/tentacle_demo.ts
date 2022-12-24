c7b8b4320b536930d134b64d52e88f24013c1ec0
lihaibo020 <lihaibo020@ke.com>
2022-12-25 02:37:09 +0800

    update

--- src/demos/tentacle_demo.ts
+++ src/demos/tentacle_demo.ts
@@ -6,6 +6,7 @@ import { getRunnerApp } from "../runnerApp";
 import { Vector } from "../vector";
 import { createDemoContext } from "../helper/demo_util";
 import { HotClass } from "../helper/class_reloader";
+import { drawSegs, updateTentacleSegs } from "../tentacle_helper";
 
 const context = createDemoContext(
     module,
@@ -51,130 +52,67 @@ const context = createDemoContext(
                 { rotate: 0, length: segLength },
             ];
 
+            const segLength1 = 60;
+
+            const segs4 = [
+                { rotate: 0, length: segLength1 },
+                { rotate: 0, length: segLength1 * 0.9 },
+                { rotate: 0, length: segLength1 * 0.8 },
+                { rotate: 0, length: segLength1 * 0.7 },
+            ];
+
+            function updateTentacleSwingSegs(root: Vector, target: Vector, startRad: number, segs: { rotate: number, length: number }[]) {
+                const rad = Vector.AB(root, target).rad();
+                const segCount = segs.length - 1;
+
+                const waveRad = 0.05 * Math.PI;
+                const waveCount = 0.8;
+                
+                for (let index = 0; index < segs.length; index++) {
+                    const element = segs[index];
+                    element.rotate = rad + waveRad * Math.sin(waveCount * index / segCount * Math.PI * 2 + startRad);
+                }
+            }
+
+
             function drawPoint(graphic: Graphics, point: Vector, color: number = 0x00ff00, radius: number = 5 ) {
                 graphic.beginFill(color);
                 graphic.drawCircle(point.x, point.y, radius);
                 graphic.endFill();
             }
 
-            function updateSegs(root: Vector, target: Vector, segs: { rotate: number, length: number }[]) {
-                const dir = target.clone().sub(root);
-                const dirLength = dir.length;
-                const dirRotate = dir.rad();
-                const segsLength = segs.reduce((prev, curr) => prev + curr.length, 0);
 
-                const _maxReFactor = 0.4;
+            updateTentacleSegs(root, target, segs);
+            updateTentacleSegs(root, target2, segs1);
+            updateTentacleSegs(root, target3, segs2);
+            updateTentacleSegs(root, target2, segs3);
 
-                const diffLength = dirLength - segsLength;
-                if (diffLength >= 0) {
-                    for (let index = 0; index < segs.length; index++) {
-                        const element = segs[index];
-                        element.rotate = dirRotate;
-                    }
-                } else {
-                    // wrap segs rotate
-                    const halfCount = Math.floor(segs.length / 2);
-                    if (segs.length % 2 === 0) {
-                        const deltaLength = dirLength / segs.length;
-                        // deltaLength * (1 - refactor) < segLength
-                        // 1 - refactor < segLength / deltaLength
-                        // refactor > 1 - segLength / deltaLength
-                        const maxReFactor = Math.min(_maxReFactor, (segLength / deltaLength - 1) * 0.8);
-                        for (let index = 0; index < segs.length; index++) {
-                            const element = segs[index];
-                            if (index >= halfCount) {
-                                // 1 + (index - halfCount) / (halfCount - 1) 
-                                // (index - 1) / (halfCount - 1)
-                                const refactor = halfCount > 1 ? (maxReFactor * Math.cos(Math.PI * (index - 1) / (halfCount - 1))) : 0;
-                                const segsRotatePerSeg = Math.acos(deltaLength * (1 - refactor) / segLength);
-                                element.rotate = segsRotatePerSeg + dirRotate;
-                                if (isNaN(element.rotate)) {
-                                    // debugger
-                                }
-                            } else {
-                                const refactor = halfCount > 1 ? (maxReFactor * Math.cos(Math.PI * index / (halfCount - 1))) : 0;
-                                const segsRotatePerSeg = Math.acos(deltaLength * (1 - refactor) / segLength);
-                                element.rotate = - segsRotatePerSeg + dirRotate;
-                                if (isNaN(element.rotate)) {
-                                    // debugger
-                                }
-                            }
-                        }
-                    } else {
-                        const evenDiffLength = dirLength - segLength;
-                        const deltaLength = Math.abs(evenDiffLength / (segs.length - 1));
-                        const maxReFactor = Math.min(_maxReFactor, (segLength / deltaLength - 1) * 0.8);
-                        const dir = evenDiffLength > 0 ? 1 : -1;
-                        for (let index = 0; index < segs.length; index++) {
-                            const element = segs[index];
-                            console.log(index, halfCount, (index - 2) / (halfCount - 1), index / (halfCount - 1));
-
-                            if (index > halfCount) {
-                                // console.log(index, halfCount, (index - 1) / halfCount );
-                                // 1 + (index - halfCount - 1) / (halfCount - 1)
-                                // (index - 2) / (halfCount - 1)
-                                const refactor = halfCount > 1 ? (maxReFactor * Math.cos(Math.PI * (index - 2) / (halfCount - 1))) : 0;
-                                const segsRotatePerSeg = Math.acos(deltaLength * (1 - refactor) / segLength);
-                                element.rotate = segsRotatePerSeg * dir + dirRotate;
-                            } else if (index < (segs.length - 1) / 2) {
-                                const refactor = halfCount > 1 ? (maxReFactor * Math.cos(Math.PI * index / (halfCount - 1))) : 0;
-                                const segsRotatePerSeg = Math.acos(deltaLength * (1 - refactor) / segLength);
-                                element.rotate = - segsRotatePerSeg * dir + dirRotate;
-                            } else {
-                                element.rotate = dirRotate;
-                            }
-
-                        }
-                    }
-                }
-            }
-
-            updateSegs(root, target, segs);
-            updateSegs(root, target2, segs1);
-            updateSegs(root, target3, segs2);
-            updateSegs(root, target2, segs3);
-
+            updateTentacleSwingSegs(root, target3, 0, segs4);
             const segsEl = animateContainer.addChild(new Graphics());
 
-            function drawSegs(graphic: Graphics, segs: { rotate: number, length: number }[]) {
-                let nextPoint = root.clone();
-                graphic.beginFill(0xff0000)
-                    .drawCircle(nextPoint.x, nextPoint.y, 3)
-                    .endFill()
 
-                for (let index = 0; index < segs.length; index++) {
-                    const element = segs[index];
-                    const _nextPoint = nextPoint.clone()
-                        .add(new Vector(
-                            Math.cos(element.rotate) * element.length,
-                            Math.sin(element.rotate) * element.length
-                        ));
-                    graphic.beginFill(0xff0000)
-                        .drawCircle(_nextPoint.x, _nextPoint.y, 3)
-                        .endFill()
-                    graphic
-                        .lineStyle(1, 0xff0000)
-                        .moveTo(nextPoint.x, nextPoint.y)
-                        .lineTo(
-                            _nextPoint.x,
-                            _nextPoint.y,
-                        );
-                    nextPoint = _nextPoint;
-                }
-            }
             segsEl.clear()
-            drawSegs(segsEl, segs);
-            drawSegs(segsEl, segs1);
-            drawSegs(segsEl, segs2);
-            drawSegs(segsEl, segs3);
-
-            drawPoint(segsEl, target3);
+            // drawSegs(segsEl, root, segs);
+            // drawSegs(segsEl, root, segs1);
+            // drawSegs(segsEl, root, segs2);
+            // drawSegs(segsEl, root, segs3);
+            drawSegs(segsEl, root, segs4);
+            drawPoint(segsEl, target2);
 
             const targetEl = animateContainer.addChild(new Graphics());
             targetEl.beginFill(0x00ff00)
                 .drawCircle(target.x, target.y, 3)
 
+            let last = 0;
+            let startRad = 0;
             return function () {
+                last ++;
+                startRad += 0.1 * Math.PI / 6;
+                updateTentacleSwingSegs(root, target3, startRad, segs4);
+
+                segsEl.clear()
+                drawSegs(segsEl, root, segs4);
+                drawPoint(segsEl, target2);
 
             }
         }
---
+++ src/tentacle_helper.ts
@@ -0,0 +1,102 @@
+import { Graphics } from "pixi.js";
+import { Vector } from "./vector";
+
+export function drawSegs(graphic: Graphics, root: Vector, segs: { rotate: number, length: number }[]) {
+    let nextPoint = root.clone();
+    graphic.beginFill(0xff0000)
+        .drawCircle(nextPoint.x, nextPoint.y, 3)
+        .endFill()
+
+    for (let index = 0; index < segs.length; index++) {
+        const element = segs[index];
+        const _nextPoint = nextPoint.clone()
+            .add(new Vector(
+                Math.cos(element.rotate) * element.length,
+                Math.sin(element.rotate) * element.length
+            ));
+        graphic.beginFill(0xff0000)
+            .drawCircle(_nextPoint.x, _nextPoint.y, 3)
+            .endFill()
+        graphic
+            .lineStyle(1, 0xff0000)
+            .moveTo(nextPoint.x, nextPoint.y)
+            .lineTo(
+                _nextPoint.x,
+                _nextPoint.y,
+            );
+        nextPoint = _nextPoint;
+    }
+}
+
+
+export function updateTentacleSegs(root: Vector, target: Vector, segs: { rotate: number, length: number }[]) {
+    const dir = target.clone().sub(root);
+    const dirLength = dir.length;
+    const dirRotate = dir.rad();
+    const segsLength = segs.reduce((prev, curr) => prev + curr.length, 0);
+
+    const _maxReFactor = 0.4;
+    const segLength = segs[0].length;
+    const diffLength = dirLength - segsLength;
+    if (diffLength >= 0) {
+        for (let index = 0; index < segs.length; index++) {
+            const element = segs[index];
+            element.rotate = dirRotate;
+        }
+    } else {
+        // wrap segs rotate
+        const halfCount = Math.floor(segs.length / 2);
+        if (segs.length % 2 === 0) {
+            const deltaLength = dirLength / segs.length;
+            // deltaLength * (1 - refactor) < segLength
+            // 1 - refactor < segLength / deltaLength
+            // refactor > 1 - segLength / deltaLength
+            const maxReFactor = Math.min(_maxReFactor, (segLength / deltaLength - 1) * 0.8);
+            for (let index = 0; index < segs.length; index++) {
+                const element = segs[index];
+                if (index >= halfCount) {
+                    // 1 + (index - halfCount) / (halfCount - 1) 
+                    // (index - 1) / (halfCount - 1)
+                    const refactor = halfCount > 1 ? (maxReFactor * Math.cos(Math.PI * (index - 1) / (halfCount - 1))) : 0;
+                    const segsRotatePerSeg = Math.acos(deltaLength * (1 - refactor) / segLength);
+                    element.rotate = segsRotatePerSeg + dirRotate;
+                    if (isNaN(element.rotate)) {
+                        // debugger
+                    }
+                } else {
+                    const refactor = halfCount > 1 ? (maxReFactor * Math.cos(Math.PI * index / (halfCount - 1))) : 0;
+                    const segsRotatePerSeg = Math.acos(deltaLength * (1 - refactor) / segLength);
+                    element.rotate = - segsRotatePerSeg + dirRotate;
+                    if (isNaN(element.rotate)) {
+                        // debugger
+                    }
+                }
+            }
+        } else {
+            const evenDiffLength = dirLength - segLength;
+            const deltaLength = Math.abs(evenDiffLength / (segs.length - 1));
+            const maxReFactor = Math.min(_maxReFactor, (segLength / deltaLength - 1) * 0.8);
+            const dir = evenDiffLength > 0 ? 1 : -1;
+            for (let index = 0; index < segs.length; index++) {
+                const element = segs[index];
+                console.log(index, halfCount, (index - 2) / (halfCount - 1), index / (halfCount - 1));
+
+                if (index > halfCount) {
+                    // console.log(index, halfCount, (index - 1) / halfCount );
+                    // 1 + (index - halfCount - 1) / (halfCount - 1)
+                    // (index - 2) / (halfCount - 1)
+                    const refactor = halfCount > 1 ? (maxReFactor * Math.cos(Math.PI * (index - 2) / (halfCount - 1))) : 0;
+                    const segsRotatePerSeg = Math.acos(deltaLength * (1 - refactor) / segLength);
+                    element.rotate = segsRotatePerSeg * dir + dirRotate;
+                } else if (index < (segs.length - 1) / 2) {
+                    const refactor = halfCount > 1 ? (maxReFactor * Math.cos(Math.PI * index / (halfCount - 1))) : 0;
+                    const segsRotatePerSeg = Math.acos(deltaLength * (1 - refactor) / segLength);
+                    element.rotate = - segsRotatePerSeg * dir + dirRotate;
+                } else {
+                    element.rotate = dirRotate;
+                }
+
+            }
+        }
+    }
+}
